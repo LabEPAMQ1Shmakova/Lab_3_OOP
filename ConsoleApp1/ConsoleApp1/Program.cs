6bb30e9a5c42e58e5e663374c2dae52025d9bfc2
juksonya <juksonya@gmail.com>
2018-04-20 23:48:09 +0300

    new com

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace Lab_3_OOP
 {
-    class Program
+    class Prog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kerosene Kerosene = new Kerosene(100, 50, "Керосин ТС-1");
+            Kerosene.Dencity();
+            Petrol Petrol = new Petrol(100, 75, "Автомобильный бензин АИ-95");
+            Petrol.Dencity();
+            DieselFluel DieselFluel = new DieselFluel(100, 85, "Дизельное топливо ДТ62");
+            DieselFluel.Dencity();
+
+
+
+            Console.ReadLine();
         }
     }
 }
